489d1c5ca55e8cc7a7e4e6b5364ca2213acae314
suparwincasino <suparwincasino@gmail.com>
2026-04-01 17:59:23 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
+// Back4App / Parse SDK import
+import Parse from "parse";
+
+// Initialize Back4App (Parse) credentials
+Parse.initialize(
+  "SlimfTCbDWxGGFfP3GVBbZFQLYB2jFGK7zAsNam6", // Application ID
+  "R2Wck7rxKoJ2MorzJHV9cFDa7jCgCaPsazVSis8K"  // JS Key
+);
+Parse.serverURL = "https://parseapi.back4app.com";
+
+// Mount React App
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
