3c753dcfa3db9c2da3b36ceb4f56330c21d1171f
suparwincasino <suparwincasino@gmail.com>
2026-04-01 18:03:14 +0600

    Update back4app.js

--- src/services/back4app.js
+++ src/services/back4app.js
@@ -1,1 +1,8 @@
+import Parse from "parse/dist/parse.min.js";
 
+Parse.initialize(
+  "SlimfTCbDWxGGFfP3GVBbZFQLYB2jFGK7zAsNam6",
+  "R2Wck7rxKoJ2MorzJHV9cFDa7jCgCaPsazVSis8K"
+);
+Parse.serverURL = "https://parseapi.back4app.com";
+export default Parse;
